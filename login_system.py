471ad14d9d263d4ccc0a1f4dce9896d6018301df
Ayesha Mumtaz <ayeshamumtaz1057@gmail.com>
2026-06-23 22:26:09 +0500

    Implement simple login system

---
+++ login_system.py
@@ -0,0 +1,9 @@
+# Simple Login System
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Welcome")
+else:
+    print("Invalid Login")
