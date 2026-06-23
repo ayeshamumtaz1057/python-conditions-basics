6834a63d77b6fd04d28c5815091486e52864178f
Ayesha Mumtaz <ayeshamumtaz1057@gmail.com>
2026-06-23 22:23:41 +0500

    Add elif condition for grading system

---
+++ elif_condition.py
@@ -0,0 +1,12 @@
+# Elif Condition
+
+marks = int(input("Enter marks: "))
+
+if marks >= 80:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+elif marks >= 40:
+    print("Grade C")
+else:
+    print("Fail")
